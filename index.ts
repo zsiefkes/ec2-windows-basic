d29504bb5f21cb736beae435c43abf2a8c172fb8
Zachary Siefkes <zachary.siefkes@sentify.co>
2022-02-18 15:20:37 +1300

    Get AMI from SSM Parameter store and create EC2 instance

--- index.ts
+++ index.ts
@@ -1,2 +1,14 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
+
+// Look up AMI based on SSM Parameter name
+const ami = pulumi.output(aws.ssm.getParameter({ name: "/aws/service/ami-windows-latest/Windows_Server-2019-English-Full-Base" }));
+
+// Create EC2 Instance
+const ec2Instance = new aws.ec2.Instance("windows-machine", {
+  ami: ami.value,
+  instanceType: "t3.large"
+});
+
+// output machine address
+export const instanceArn = ec2Instance.arn;
