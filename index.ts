5c084374acbbfa3875c033451432258f970da81e
Zachary Siefkes <zachary.siefkes@sentify.co>
2022-02-21 18:50:33 +1300

    Add key pair to instance

--- index.ts
+++ index.ts
@@ -1,14 +1,29 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
+import * as tls from "@pulumi/tls";
 
 // Look up AMI based on SSM Parameter name
 const ami = pulumi.output(aws.ssm.getParameter({ name: "/aws/service/ami-windows-latest/Windows_Server-2019-English-Full-Base" }));
 
+// Create Key Pair
+const keyPairName = "windows-machine-key-pair";
+const tlsKey = new tls.PrivateKey("tls-key", {
+  algorithm: "RSA"
+});
+const keyPair = new aws.ec2.KeyPair(keyPairName, {
+  publicKey: tlsKey.publicKeyOpenssh,
+  keyName: keyPairName
+});
+
 // Create EC2 Instance
 const ec2Instance = new aws.ec2.Instance("windows-machine", {
   ami: ami.value,
-  instanceType: "t3.large"
+  instanceType: "t3.large",
+  keyName: keyPairName
 });
 
-// output machine ip address
+// Output machine IP address
 export const ipAddress = ec2Instance.publicIp;
+// Output key pair
+export const privateKey = tlsKey.privateKeyPem;
+export const publicKey = tlsKey.privateKeyPem;
