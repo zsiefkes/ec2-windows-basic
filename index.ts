cd21146e28a915d5c99c9eba92f4cada1cf012f0
Zachary Siefkes <zachary.siefkes@sentify.co>
2022-02-24 10:19:57 +1300

    Switch to ubuntu image and add IAM role

--- index.ts
+++ index.ts
@@ -2,12 +2,15 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as tls from "@pulumi/tls";
 
+// Stack name
+const name = "ubuntu-migration-server";
+
 // Look up AMI based on SSM Parameter name
-const ami = pulumi.output(aws.ssm.getParameter({ name: "/aws/service/ami-windows-latest/Windows_Server-2019-English-Full-Base" }));
+const ami = pulumi.output(aws.ssm.getParameter({ name: "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2" }));
 
 // Create Key Pair
-const keyPairName = "windows-machine-key-pair";
-const tlsKey = new tls.PrivateKey("tls-key", {
+const keyPairName = `${name}-key-pair`;
+const tlsKey = new tls.PrivateKey(name, {
   algorithm: "RSA"
 });
 const publicKey = tlsKey.publicKeyOpenssh;
@@ -16,11 +19,33 @@ const keyPair = new aws.ec2.KeyPair(keyPairName, {
   keyName: keyPairName
 });
 
+// Create IAM role and attach policies (SSM related)
+const assumeRolePolicy: aws.iam.PolicyDocument = {
+  Version: "2012-10-17",
+  Statement: [{
+    Action: ["sts:AssumeRole"],
+    Effect: "Allow",
+    Principal: { Service: "ec2.amazonaws.com" }
+  }],
+};
+const iamRole = new aws.iam.Role(name, {
+  assumeRolePolicy: assumeRolePolicy
+});
+new aws.iam.RolePolicyAttachment(name, {
+  role: iamRole,
+  policyArn: aws.iam.ManagedPolicy.AmazonSSMManagedInstanceCore
+});
+// Create instance profile from the role
+const iamInstanceProfile = new aws.iam.InstanceProfile(name, {
+  role: iamRole
+});
+
 // Create EC2 Instance
-const ec2Instance = new aws.ec2.Instance("windows-machine", {
+const ec2Instance = new aws.ec2.Instance(name, {
   ami: ami.value,
   instanceType: "t3.large",
-  keyName: keyPairName
+  keyName: keyPairName,
+  iamInstanceProfile: iamInstanceProfile
 });
 
 // Output machine IP address
